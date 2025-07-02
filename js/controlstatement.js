c2c245c15c4c7a94970dbe46773c70c749f9e73c
MarkSoyiri <marksoyiri001@gmail.com>
2025-07-02 15:39:48 +0100

    update2

---
+++ js/controlstatement.js
@@ -0,0 +1,16 @@
+// WRITE A PROGRAM TO CHECK IF SOMEONE CAN VOTE
+// DATA
+let age = 15;
+// LOGIC @TRUE OR FALSE
+let canVote = age >= 18;
+// CHECK(CINTROL)
+if(canVote){
+
+    console.log("You can vote")
+
+}else{
+
+    console.log("You cant vote")
+}
+            // ASSIGNMENT
+// WRITE A PROGRAM TO SHARE MONEY ACCORDING BASED ON AGE, OLDER GETS A HIGHER AMOUNT
--- js/soscomtest.js
+++ js/soscomtest.js
@@ -1,10 +1,16 @@
-const startbox = document.getElementById('startbox')
+let startbox = document.getElementById('startbox')
+let startbox1 = document.getElementById('startbox1')
 const loginform = document.getElementById('loginform')
 const registerform = document.getElementById('registerform')
 let gotoregister = document.getElementById('gotoregister')
 let gotologin = document.getElementById('gotologin')
 
 startbox.onclick = function (){
+
+    loginform.classList.toggle('swapDisplay')
+    registerform.classList.remove('swapDisplay')
+}
+startbox1.onclick = function (){
 
     loginform.classList.toggle('swapDisplay')
     registerform.classList.remove('swapDisplay')
