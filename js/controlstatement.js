a206eaad2dc71f07a78ee4447a3e13e19ac482a2
MarkSoyiri <marksoyiri001@gmail.com>
2025-07-07 00:49:32 +0100

    Update3

--- js/controlstatement.js
+++ js/controlstatement.js
@@ -4,13 +4,72 @@ let age = 15;
 // LOGIC @TRUE OR FALSE
 let canVote = age >= 18;
 // CHECK(CINTROL)
-if(canVote){
+// if(canVote){
 
-    console.log("You can vote")
+//     console.log("You can vote")
 
-}else{
+// }else{
 
-    console.log("You cant vote")
-}
+//     console.log("You cant vote")
+// }
             // ASSIGNMENT
-// WRITE A PROGRAM TO SHARE MONEY ACCORDING BASED ON AGE, OLDER GETS A HIGHER AMOUNT
+// WRITE A PROGRAM TO SHARE MONEY AMONGST THREE PEOPLE BASED ON AGE, OLDER GETS A HIGHER AMOUNT
+
+
+// let first = 1
+// let second = 17
+// let third = 7
+
+
+// let firstgreat = first > second && first >third;
+// let secondgreat = second > first && second > third;
+// let thirdgreat = third > first && third > second;
+
+
+// firstgreat ? console.log('Higest share') :'';
+// secondgreat ? console.log('Second Highest share') :'';
+// thirdgreat ? console.log('Lowest share') :'';
+
+
+
+// let first = 45;
+// let second =20 ;
+// let third = 17;
+
+
+// let firstperson = first > 30;
+// let secondperson = second < 30 ||second >= 18;
+// let thirdperson = third >= 12 || third < 18;
+// if(firstperson){
+//     console.log('Highest share');
+// }else{
+//     console.log('');
+// }
+// if(secondperson){
+//     console.log('Second Highest');
+// }else{
+//     console.log('');
+// }
+// if(thirdperson){
+//     console.log('Lowest share');
+// }else{
+//     console.log('');
+// }
+
+let person = 15;
+
+person >= 30 ? console.log('Highest share'): '';
+person < 30 && person >= 18 ? console.log('Second highest share'): '';
+person < 18 ? console.log('Lowest share') : '';
+
+
+
+
+
+
+
+
+
+// firstgreat ? console.log('Higest share') :'';
+// secondgreat ? console.log('Second Highest share') :'';
+// thirdgreat ? console.log('Lowest share') :'';
