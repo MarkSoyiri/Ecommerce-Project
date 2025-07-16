8166636cff2018c7f3edfeeb7774b32ef1671b7f
MarkSoyiri <marksoyiri001@gmail.com>
2025-07-16 17:48:00 +0100

    Pricing PAGE Update

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,63 @@
-// 1.  Anonymous function
-let MyFunction = function (){console.log('Anonymous')} 
+// // 1.  Anonymous function
+// let MyFunction = function (){console.log('Anonymous')} 
 
-// 2. Arrow Function
-let MyArrowFunction = ()=>{console.log('Arrow')}
+// // 2. Arrow Function
+// let MyArrowFunction = ()=>{console.log('Arrow')}
 
-// 3. Named Function
-let NamedFunction = function NameOfFunction9(){console.log('NamedFunction')}
+// // 3. Named Function
+// let NamedFunction = function NameOfFunction9(){console.log('NamedFunction')}
 
-// Calling A Function
+// // Calling A Function
 
-let run = document.getElementById('run_test')
+// let run = document.getElementById('run_test')
 
-run_test.onclick = MyFunction
+// run.onclick = MyFunction
+
+
+let Engineers = [
+    {
+        name:"Michelle",
+        email:"Michelle@gmail.com",
+        password:"Michelle12345"
+    },{
+        name:"Michael",
+        email:"Michelle@gmail.com",
+        password:"Michael12345"
+    },{
+        name:"Mark",
+        email:"Michelle@gmail.com",
+        password:"Mark12345"
+    },{
+        name:"George",
+        email:"Michelle@gmail.com",
+        password:"George12345"
+    }
+]
+
+
+let form = document.getElementById('form');
+let login = document.getElementById('login');
+
+function SendData(event){
+
+
+event.preventDefault();
+let formData = new FormData(form)       // [new FormData(id of the form)] this is a constant used in storing incoming data
+let getData = Object.fromEntries(formData.entries())    //Object.fromEntries reps received data /formData reps stored data
+
+console.log(getData.email)
+console.log(getData.password)
+
+SignIn(getData.email,getData.password)
+}
+
+login.onclick = SendData
+
+
+function SignIn(p,g){
+
+    alert(p+g)
+    
+}
+
+
