447ccad56b2249d70245b8a4a06a8b3d7c8a2cda
MarkSoyiri <marksoyiri001@gmail.com>
2025-07-20 20:21:19 +0100

    Enterprise Page Update1

--- js/functions.js
+++ js/functions.js
@@ -16,48 +16,95 @@
 
 let Engineers = [
     {
-        name:"Michelle",
-        email:"Michelle@gmail.com",
-        password:"Michelle12345"
-    },{
-        name:"Michael",
-        email:"Michelle@gmail.com",
-        password:"Michael12345"
-    },{
-        name:"Mark",
-        email:"Michelle@gmail.com",
-        password:"Mark12345"
-    },{
-        name:"George",
-        email:"Michelle@gmail.com",
-        password:"George12345"
+        name: "Michelle",
+        email: "Michelle@gmail.com",
+        password: "Michelle12345"
+    }, {
+        name: "Michael",
+        email: "Michael@gmail.com",
+        password: "Michael12345"
+    }, {
+        name: "Mark",
+        email: "Mark@gmail.com",
+        password: "Mark12345"
+    }, {
+        name: "George",
+        email: "George@gmail.com",
+        password: "George12345"
     }
 ]
-
+console.log(Engineers)
 
 let form = document.getElementById('form');
 let login = document.getElementById('login');
 
-function SendData(event){
+function SendData(event) {
 
 
-event.preventDefault();
-let formData = new FormData(form)       // [new FormData(id of the form)] this is a constant used in storing incoming data
-let getData = Object.fromEntries(formData.entries())    //Object.fromEntries reps received data /formData reps stored data
+    event.preventDefault();
+    let formData = new FormData(form)       // [new FormData(id of the form)] this is a constant used in storing incoming data
+    let getData = Object.fromEntries(formData.entries())    //Object.fromEntries reps received data /formData reps stored data
 
-console.log(getData.email)
-console.log(getData.password)
+    console.log(getData.email)
+    console.log(getData.password)
 
-SignIn(getData.email,getData.password)
+    let Successful = SignIn(getData.email, getData.password)
+
+   if ( Successful = undefined) {
+
+     formbox.innerHTML = SignIn(getData.email, getData.password)
+    
+   } else {
+
+     alert("Login Failed")
+    
+   }
+   
+
+    // if (SignIn(getData.email, getData.password)==="undefined") {
+    //     alert("Not Found")
+        
+    // }
 }
 
 login.onclick = SendData
 
 
-function SignIn(p,g){
 
-    alert(p+g)
+// function SignIn(email, password) {
+
+//    try {
+//      for (let index = 0; index <= Engineers.length; index++) {
+//         const element = Engineers[index]
+
+//         if (element.email === email && element.password === password) {
+
+//             // console.log("true")
+//             // alert("Login Successful")
+            
+//             return `<h3 style=" color:yellow;font-size:45px;">Login Successful!!! <br> Welcome ${element.name}</h3>`
+        
+//         } 
+//     }
     
-}
+//    } catch (error) {
+//         alert("Login Failed")
+//    }
 
 
+
+
+// } 
+
+for (let index = 0; index <= Engineers.length; index++) {
+        const element = Engineers[index]
+
+        if (element.email === email && element.password === password) {
+
+            // console.log("true")
+            // alert("Login Successful")
+            
+            return `<h3 style=" color:yellow;font-size:45px;">Login Successful!!! <br> Welcome ${element.name}</h3>`
+        
+        } 
+    }
